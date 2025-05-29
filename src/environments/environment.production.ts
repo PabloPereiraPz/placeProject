f142601d527deea58dc342b7ea268c54b8789e78
PabloPcCap <pablo.a.cruz@capgemini.com>
2025-05-28 21:18:24 -0300

    Update deployment configuration for Vercel

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://www.postman.com/pablo-5634870/pablo-s-workspace/collection/qwcn6l7/curso-angular', // URL de produção
-    localApiUrl: 'http://localhost:4000' // URL local (caso precise testar em produção localmente)
+    apiUrl: 'https://place-project.vercel.app/api', // Production API URL
+    localApiUrl: 'http://localhost:4000' // Local testing URL
 };

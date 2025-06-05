e6c489cdfb053468c0776dba95f82c99635cadfc
PabloPcCap <pablo.a.cruz@capgemini.com>
2025-06-04 21:36:06 -0300

    feat: correção bug 404

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router(require('./db.production.json'));
+const path = require('path');
+const router = jsonServer.router(path.join(__dirname, 'db.production.json'));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
